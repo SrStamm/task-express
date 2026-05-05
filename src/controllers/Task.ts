68941173a39ad66fbbea20114a4bb9eb696937be
SrStamm <mirkostamm19@gmail.com>
2026-05-05 20:45:13 +0100

    feat: added more controllers to Task
    - controller to get a task by id
    - controller to cerate a new task

--- src/controllers/Task.ts
+++ src/controllers/Task.ts
@@ -1,7 +1,32 @@
 import { Request, Response } from "express";
 import * as taskService from "../services/task";
+import { Task } from "../models/Task";
 
 export const getAllTasks = (req: Request, res: Response) => {
   const tasks = taskService.getAllTasks();
   return res.status(200).json(tasks);
+};
+
+export const getTaskById = (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const task = taskService.getTaskById(id);
+
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+
+  res.status(200).json(task);
+};
+
+export const createTask = (req: Request, res: Response) => {
+  const body: Task = req.body;
+
+  const task = taskService.createTask(body.text);
+
+  if (!task) {
+    return res.status(400).json({ error: "Task not created" });
+  }
+
+  res.status(201).json(task);
 };
