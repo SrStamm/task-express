424e638ed09776d702671dea1d8b29c539988bf7
SrStamm <mirkostamm19@gmail.com>
2026-05-05 20:23:45 +0100

    feat: endpoint to get a task by id added

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -12,6 +12,16 @@ tasksRouter.get("/", (req: Request, res: Response) => {
   res.send(listTasks);
 });
 
+tasksRouter.get("/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const task = listTasks.find((t) => t.id == id);
+
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+  res.status(200).json(task);
+});
+
 tasksRouter.post("/", (req: Request, res: Response) => {
   const body = req.body;
   const task: Task = body;
