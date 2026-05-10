3978c1484fa604b55861cef178d0a57e891687a3
SrStamm <mirkostamm19@gmail.com>
2026-05-10 08:28:49 +0100

    feat: added schemas for Task
    - schema for delete, create and update

--- src/modules/tasks/tasks.schema.ts
+++ src/modules/tasks/tasks.schema.ts
@@ -1,5 +1,40 @@
+import { z } from "zod";
+
 export interface Task {
   id?: number;
   userId: number;
   text: string;
 }
+
+export const createTaskRouterSchema = z.object({
+  body: z.object({
+    text: z.string(),
+  }),
+  params: z.object({
+    id: z.coerce.number(),
+  }),
+});
+
+export const createTaskSchema = z.object({
+  text: z.string(),
+  userId: z.number().int().positive(),
+});
+
+export const updateTaskSchema = z.object({
+  body: z.object({
+    text: z.string(),
+  }),
+  params: z.object({
+    id: z.coerce.number(),
+  }),
+});
+
+export const deleteTaskSchema = z.object({
+  params: z.object({
+    id: z.coerce.number(),
+  }),
+});
+
+export type CreateTaskInput = z.infer<typeof createTaskSchema>;
+export type UpdateTaskBody = z.infer<typeof updateTaskSchema>["body"];
+export type DeleteTaskParams = z.infer<typeof updateTaskSchema>["params"];
