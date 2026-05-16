0db3b3bef97e4cbec36a1a16c5aa5e53b43853b1
SrStamm <mirkostamm19@gmail.com>
2026-05-16 12:27:57 +0100

    fix: get user by params, not for user logged data

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,21 +1,24 @@
 import { Request, Response } from "express";
 import * as userService from "./users.service";
 import { CreateUserRouterSchema } from "./users.schema";
+import { cathAsync } from "../../utils/cathAsync";
 
-export const getAllusers = async (req: Request, res: Response) => {
+export const getAllusers = cathAsync(async (req: Request, res: Response) => {
   const users = await userService.getAllUsers();
   return res.status(200).json(users);
-};
+});
 
-export const getUserById = async (req: Request, res: Response) => {
-  const user = await userService.getUserById(req.user.userId);
+export const getUserById = cathAsync(async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  const user = await userService.getUserById(id);
 
   if (!user) {
-    return res.status(404).json({ error: "user not found" });
+    return res.status(404).json({ error: "User not found" });
   }
 
   res.status(200).json(user);
-};
+});
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, email }: CreateUserRouterSchema = req.body;
