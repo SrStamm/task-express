96c4e9f1ee99bbcea88cbbc1c4246d7ff739742d
SrStamm <mirkostamm19@gmail.com>
2026-05-17 18:25:23 +0100

    feat: TaskCard and TaskList added

--- frontend/src/components/Task/TaskCard.tsx
+++ frontend/src/components/Task/TaskCard.tsx
@@ -1,1 +1,14 @@
 // Task individual
+
+import type { Task } from "../../types/task";
+import "./TaskCard.css";
+
+interface TaskCardProps {
+  Task: Task;
+}
+
+function TaskCard({ Task }: TaskCardProps) {
+  return <div className="task-card">{Task.text}</div>;
+}
+
+export default TaskCard;
--- frontend/src/components/Task/TaskList.tsx
+++ frontend/src/components/Task/TaskList.tsx
@@ -1,1 +1,20 @@
 // listado de tareas
+
+import type { Task } from "../../types/task";
+import TaskCard from "./TaskCard";
+
+interface TaskListProps {
+  Tasks: Task[];
+}
+
+function TaskList({ Tasks }: TaskListProps) {
+  return (
+    <ol>
+      {Tasks.map((t) => {
+        return <TaskCard Task={t} />;
+      })}
+    </ol>
+  );
+}
+
+export default TaskList;
