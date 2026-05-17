5b88877c9c7e7e2c6a6bd1556f7d410200ab07e9
SrStamm <mirkostamm19@gmail.com>
2026-05-17 13:13:48 +0100

    feat: added react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { AuthPage } from "./pages/AuthPage";
+import "./App.css";
 
 function App() {
-  return <AuthPage />;
+  return (
+    <Routes>
+      <Route path="/auth" element={<AuthPage />} />
+    </Routes>
+  );
 }
 
 export default App;
