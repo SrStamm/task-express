72d94b579fc4dcaa852fce94d9e23fd09b417e7a
SrStamm <mirkostamm19@gmail.com>
2026-05-17 17:04:48 +0100

    feat: select project created

--- frontend/src/components/Project/ProjectHeader.tsx
+++ frontend/src/components/Project/ProjectHeader.tsx
@@ -1,12 +1,42 @@
 // Selector + info del proyecto
 
+import type { Project } from "../../types/project";
 import "./ProjectHeader.css";
 
-function ProjectHeader() {
+interface ProjectHeaderProps {
+  project: Project;
+  projectList: Project[];
+  updateSelectedProject: (id: number) => void;
+}
+
+function ProjectHeader({
+  project,
+  projectList,
+  updateSelectedProject,
+}: ProjectHeaderProps) {
   return (
     <nav className="nav-header">
-      <p>Titulo y datos del proyecto</p>
-      <p>Elemento 'select'</p>
+      <div>
+        <p>Titulo: {project.title}</p>
+        <p>Cantidad de tareas: {project.tasks.length}</p>
+      </div>
+
+      <div className="project-selector">
+        <select
+          value={project.id}
+          onChange={(e) => {
+            updateSelectedProject(+e.target.value);
+          }}
+        >
+          {projectList.map((p) => {
+            return (
+              <option value={p.id} key={p.id}>
+                {p.title}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </nav>
   );
 }
