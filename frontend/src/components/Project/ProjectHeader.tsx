82f82cc49a3e3c8e63917a02abd15e2fc68df7db
SrStamm <mirkostamm19@gmail.com>
2026-05-17 16:29:20 +0100

    feat: created ProjectHeader base

---
+++ frontend/src/components/Project/ProjectHeader.tsx
@@ -0,0 +1,14 @@
+// Selector + info del proyecto
+
+import "./ProjectHeader.css";
+
+function ProjectHeader() {
+  return (
+    <nav className="nav-header">
+      <p>Titulo y datos del proyecto</p>
+      <p>Elemento 'select'</p>
+    </nav>
+  );
+}
+
+export default ProjectHeader;
---
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -0,0 +1,14 @@
+import ProjectHeader from "../../components/Project/ProjectHeader";
+import "./DashboardPage.css";
+
+function DashboardPage() {
+  return (
+    <div className="dashboard-layout">
+      <header>
+        <ProjectHeader />
+      </header>
+    </div>
+  );
+}
+
+export default DashboardPage;
